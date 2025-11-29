12dd8c27d9657ee3472f04af2d52b16112c10ce2
Md Hasanuzzaman Rony <hasanuxaman.rony@hotmail.com>
2025-11-29 17:44:48 +0600

    Validation Filter

--- MinimalApiDemo/Program.cs
+++ MinimalApiDemo/Program.cs
@@ -1,43 +1,39 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/products", () =>
-{
-    return new List<string> { "Mouse", "Keyboard", "Monitor" };
-});
-
-app.MapGet("/products/{id}", (int id) =>
-{
-    return $"Your Product ID is: {id}";
-});
-app.MapPost("/products", (Product product) =>
-{
-    return Results.Ok(new { Message = "Product Added", product });
-});
-
-public record Product(int Id, string Name, double Price);
-
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+// Minimal API with filter
+app.MapGet("/products", GetProducts)
+   .AddEndpointFilter<MyValidationFilter>();
 
 app.Run();
+
+// Function
+List<string> GetProducts()
+{
+    return new List<string> { "Mouse", "Keyboard", "Monitor" };
+}
+
+// Validation Filter
+public class MyValidationFilter : IEndpointFilter
+{
+    public async ValueTask<object> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        // Example validation: token required
+        if (!context.HttpContext.Request.Query.ContainsKey("token"))
+        {
+            return Results.BadRequest("Token is missing");
+        }
+
+        return await next(context);
+    }
+}
